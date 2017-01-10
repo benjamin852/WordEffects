69f7a9b7515b143e70158b6a75dad8cd71a28fe4
Ben Weinberg <benweinberg@bens-macbook-pro.local>
2017-01-09 20:22:09 -0500

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Ben Weinberg on 2017-01-09.
+//  Copyright © 2017 Ben Weinberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
