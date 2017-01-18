6e7924a06fe9783b8e807fbb63c5b73182c69bc8
Ben Weinberg <weinbergben33@gmail.com>
2017-01-17 20:31:50 -0500

    Initial Commit
    This is the initial commit of the project

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -10,8 +10,39 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //225 unit long array of characters
+        char inputChars [255];
+        printf("Input a string:");
+
+        
+        //limit input to max 255 characters
+        fgets(inputChars, 255, stdin);
+        
+        //print as a c string
+        printf("your string is %s\n", inputChars);
+        
+        //convert char array to an NSString object
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        
+        //print NSString object
+        NSLog(@"Input was: %f", [inputString floatValue]);
+        NSLog (@"%@", [inputString stringByAppendingString:@"eh"]);
+        
+        if ([inputString hasSuffix:@"?"])
+            NSLog(@"I dont know");
+        
+        if ([inputString hasSuffix:@"!"])
+            NSLog (@"Whoa calm down");
+        
+        NSLog (@"%@", [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"]);
+        
+        
+        
+        
+         NSLog (@"Input a string %@", inputString.uppercaseString);
+         NSLog (@"Input a string %@", inputString.lowercaseString);
+        
+        
     }
     return 0;
 }
